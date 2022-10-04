937dd6bac98038a5e86a1c4f03866e6d0b913318
seleebe <skrslava04@gmail.com>
2022-10-04 09:16:56 +0300

    added quicksort

---
+++ QuickSort.h
@@ -0,0 +1,25 @@
+void QuickSort(vector<int>& a, int begin, int end) {
+	int i, j, d;
+	i = begin;
+	j = end;
+	int mid = a[(begin + end) / 2];
+	while (i <= j) {
+		while (a[i] < mid) {
+			i++;
+		}
+		while (a[j] > mid) {
+			j--;
+		}
+		if (i <= j) {
+			swap(a[i], a[j]);
+			i++;
+			j--;
+		}
+	}
+	if (i < end) {
+		QuickSort(a, i, end);
+	}
+	if (begin < j) {
+		QuickSort(a, begin, j);
+	}
+}
